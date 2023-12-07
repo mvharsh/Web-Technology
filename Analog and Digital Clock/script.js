045a53658efa781b39e74390d23d83c9f2e049fc
Harshini <harshinivivekm@gmail.com>
2023-12-07 23:01:14 +0530

    Add files via upload

---
+++ Analog and Digital Clock/script.js
@@ -0,0 +1,60 @@
+function updateClock() {
+    var now = new Date();
+    var dname = now.getDay(),
+        mo = now.getMonth(),
+        dnum = now.getDate(),
+        yr = now.getFullYear(),
+        hou = now.getHours(),
+        min = now.getMinutes(),
+        sec = now.getSeconds(),
+        pe = "AM";
+
+    if (hou >= 12) {
+        pe = "PM";
+    }
+    if (hou == 0) {
+        hou = 12;
+    }
+    if (hou > 12) {
+        hou = hou - 12;
+    }
+
+    Number.prototype.pad = function (digits) {
+        for (var n = this.toString(); n.length < digits; n = 0 + n);
+        return n;
+    }
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var ids = ["dayname", "month", "daynum", "year", "d_hour", "d_minutes", "d_seconds", "d_period"];
+    var values = [week[dname], months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
+    for (var i = 0; i < ids.length; i++)
+        document.getElementById(ids[i]).textContent = values[i];
+}
+
+function initClock() {
+    updateClock();
+    window.setInterval(updateClock, 1000);
+}
+
+initClock();
+
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let seconds = document.getElementById("seconds");
+
+let set_clock = setInterval(() => {
+    let date_now = new Date();
+
+    let hr = date_now.getHours();
+    let min = date_now.getMinutes();
+    let sec = date_now.getSeconds();
+
+    let calc_hr = (hr * 30) + (min / 2);
+    let calc_min = (min * 6) + (sec / 10);
+    let calc_sec = sec * 6;
+
+    hour.style.transform = `rotate(${calc_hr}deg)`;
+    minute.style.transform = `rotate(${calc_min}deg)`;
+    seconds.style.transform = `rotate(${calc_sec}deg)`;
+}, 1000);
